cc7a667d72666a59341edb7354234ea51adf951b
carmal <carmal.s@attinadsoftware.com>
2019-09-03 18:41:06 +0530

    Developer Testing of Pipeline

--- etsello-deploy.sh
+++ etsello-deploy.sh
@@ -7,7 +7,7 @@ BUILD_DIR=$2
 executor()
 {
 cd $BUILD_DIR
-echo "Current Dir : $PWD"
+pwd
 docker-compose down
 docker rmi -f $(docker images | grep $DOCKER_IMAGE | tr -s ' ' | cut -d ' ' -f 3)
 docker images
